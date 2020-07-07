a3030b31e05c2d3ed95bda2c0f8e26a2af6ec890
BenJensen2 <btjensen@mtu.edu>
2020-07-06 21:20:43 -0700

    Super Ninja

--- Javascript/Ninja.js
+++ Javascript/Ninja.js
@@ -45,35 +45,3 @@ superSensei.speakWisdom("What one programmer can do in one month, two programmer
 // -> "What one programmer can do in one month, two programmers can do in two months."
 superSensei.showStats();
 // -> "Name: Master Splinter, Health: 210, Speed: 10, Strength: 10"
-
-// // parent Vehicle class
-// class Vehicle {
-//     constructor(manufacturer, model, color) {
-//         this.manufacturer = manufacturer;
-//         this.model = model;
-//         this.color = color;
-//         this.miles = 0;
-//     }
-//     drive() {
-//         this.miles += 10;
-//         console.log(`You drove your ${ this.constructor.name } and it now has ${this.miles} miles on it.`);
-//     }
-//     // simple method in the parent class
-//     parentFunction(){
-//         return "This is coming from the parent!";
-//     }
-// }
-// // child M5 class
-// class M5 extends Vehicle {
-//     constructor(color) {
-//         super("BMW", "M5", color);
-//     }
-//     // simple function in the child class
-//     childFunction() {
-//         // by using super, we can call the parent method
-//         const message = super.parentFunction();
-//         console.log(message);
-//     }
-// }
-// const m5 = new M5("Blue");
-// m5.childFunction();
