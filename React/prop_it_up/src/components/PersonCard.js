f0637ee70f6c2b5122da4a8f98b95b5962a17f0a
BenJensen2 <btjensen@mtu.edu>
2020-07-08 13:46:17 -0700

    Big Inversion Complete

--- React/prop_it_up/src/components/PersonCard.js
+++ React/prop_it_up/src/components/PersonCard.js
@@ -1,27 +1,43 @@
-import React, { Component } from 'react';
+import React from 'react';
 
-class PersonCard extends Component {
+// // Class version
+// class PersonCard extends Component {
 
-constructor(props) {
-  super(props);
-  this.state = {
-    age: this.props.age
-  };
-}
-  increaseAge = () => {
-    this.setState({ age: this.state.age + 1});
-}
-render() {
-    return <div>
-       {/* The Person */}
-       {/* {this.props.theText} */}
+// constructor(props) {
+//   super(props);
+//   this.state = {
+//     age: this.props.age
+//   };
+// }
+//   increaseAge = () => {
+//     this.setState({ age: this.state.age + 1});
+// }
+// render() {
+//     return <div>
+//        {/* The Person */}
+//        {/* {this.props.theText} */}
 
-       <h1>{this.props.lastName} , {this.props.firstName} </h1>
-       <p>Age: {this.state.age}</p>
-       <p>Hair Color: {this.props.hairColor}</p>
-       <button onClick={ this.increaseAge } >Birthday Button for {this.props.firstName} {this.props.lastName}</button>
-     </div>
-  }
+//        <h1>{this.props.lastName} , {this.props.firstName} </h1>
+//        <p>Age: {this.state.age}</p>
+//        <p>Hair Color: {this.props.hairColor}</p>
+//        <button onClick={ this.increaseAge } >Birthday Button for {this.props.firstName} {this.props.lastName}</button>
+//      </div>
+//   }
+// }
+
+// Functional Components
+const PersonCard = props => {
+  return(
+        <div>
+         {/* The Person */}
+         {/* {this.props.theText} */}
+  
+         <h1>{props.lastName} , {props.firstName} </h1>
+         <p>Age: {props.age}</p>
+         <p>Hair Color: {props.hairColor}</p>
+         {/* <button onClick={ increaseAge } >Birthday Button for {props.firstName} {this.props.lastName}</button> */}
+       </div>
+  );
 }
 
 export default PersonCard;
